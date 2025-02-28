2be13f161d1343b5fc79fd23e083c40d762be966
Inggrit Setya Budi <115447070+Setyabudi90@users.noreply.github.com>
2025-03-01 00:11:17 +0700

    Update video-player.tsx

--- app/watch/[id]/video-player.tsx
+++ app/watch/[id]/video-player.tsx
@@ -126,7 +126,6 @@ export default function VideoPlayer({
               <img
                 src={blobImage || channelDetails.snippet.thumbnails.default.url}
                 alt={channelDetails.snippet.title}
-                loading="eager"
                 className="!rounded-full w-[40px] h-[40px] flex-shrink-0"
               />
               <Link href={`/channel/${channelDetails.id}`}>
